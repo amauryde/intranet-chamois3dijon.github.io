1b3d95ca498cd87bd59feee66717992f4238864b
Amaury D <77858903+amauryde@users.noreply.github.com>
2024-05-07 22:07:37 +0200

    Add files via upload

---
+++ config.inc.php
@@ -0,0 +1,8 @@
+<?php
+//ESPACE DISQUE / NB USERS / DB CONFIG
+define("limite_espace_disque", "10737418240");
+define("limite_nb_users", "10000");
+define("db_host", "");
+define("db_name", "");
+define("db_login", "");
+define("db_password", '');
